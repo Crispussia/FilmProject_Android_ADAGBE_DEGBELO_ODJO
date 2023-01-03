ead5a8ae1fdd9d7263bda68e52fb994ed4b9278a
Inebur <mahugnonbruneiadagbe@gmail.com>
2023-01-03 04:35:08 +0100

    Testing LinearLayout in FrameLayout

--- app/src/main/java/fi/eilco/android_project_gestion_film/MainActivity.kt
+++ app/src/main/java/fi/eilco/android_project_gestion_film/MainActivity.kt
@@ -2,10 +2,17 @@ package fi.eilco.android_project_gestion_film
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fi.eilco.android_project_gestion_film.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // push our fragment on our fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/fi/eilco/android_project_gestion_film/fragments/HomeFragment.kt
@@ -0,0 +1,19 @@
+package fi.eilco.android_project_gestion_film.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fi.eilco.android_project_gestion_film.R
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
